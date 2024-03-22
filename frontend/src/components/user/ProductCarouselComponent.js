bef2d583647807d76ef4b8527962d36e7de17f83
or-imon <orimon9@gmail.com>
2024-03-22 22:47:06 +0200

    Homepage degin and customization

--- frontend/src/components/user/ProductCarouselComponent.js
+++ frontend/src/components/user/ProductCarouselComponent.js
@@ -1,25 +1,33 @@
-import Carousel from 'react-bootstrap/Carousel';
-//import ExampleCarouselImage from 'components/ExampleCarouselImage';
+import { Carousel } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const ProductCarouselComponent =() =>{
+    const cursorP = {
+        cursor: "pointer",
+      };
 return (
     <Carousel>
       <Carousel.Item>
       <img
+          crossorigin="anonymous"
           className="d-block w-100"
-          src="/images/carousel/carousel-1.png"
+          style={{ height: "300px", objectFit: "cover" }}
+          src="/images/carousel/analog-classic.jpg"
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+       <Carousel.Caption>
+          <LinkContainer style={cursorP} to="/product-details">
+            <h3>Bestseller in Laptops Category</h3>
+          </LinkContainer>
+          <p>Dell Inspiron 15 3000 Laptop, 15.6 inch HD</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
 
       <img
           className="d-block w-100"
-          src="/images/carousel/carousel-2.png"
+          style={{ height: "300px", objectFit: "cover" }}
+          src="/images/carousel/analog-classic.jpg"
           alt="Second slide"
         />
         <Carousel.Caption>
@@ -31,30 +39,40 @@ return (
 
       <img
           className="d-block w-100"
-          src="/images/carousel/carousel-2.png"
+          style={{ height: "300px", objectFit: "cover" }}
+          src="/images/carousel/cup-on-a-table.jpg"
           alt="Second slide"
         />
+
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <LinkContainer style={cursorP} to="/product-details">
+            <h3>Bestseller in Books Category</h3>
+          </LinkContainer>
+          <p>World of Eric Carle, Hear Bear Roar 30-Button Animal Sound Book</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
+
         
-        <img
+      <img
           className="d-block w-100"
-          src="/images/carousel/carousel-2.png"
-          alt="Second slide"
+          style={{ height: "300px", objectFit: "cover" }}
+          src="/images/carousel/keyboard.jpg"
+          alt="Third slide"
         />
+
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <LinkContainer style={cursorP} to="/product-details">
+            <h3>Bestseller in Cameras Category</h3>
+          </LinkContainer>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            4K Camcorder Video Camera 60FPS 48MP Vlogging Camera for YouTube
+            WiFi 16X Digital Camera
           </p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
-}
+};
 
 export default ProductCarouselComponent;
