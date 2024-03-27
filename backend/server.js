4144900e110770b54a2061976258d7bb87343567
or-imon <orimon9@gmail.com>
2024-03-27 23:08:20 +0200

    MongoDb config,Mongoose to connect with MongoDB,server config

--- backend/server.js
+++ backend/server.js
@@ -5,21 +5,12 @@ const port = 3000
 const apiRoutes = require("./routes/apiRoutes")
 
 app.get('/', (req, res) => {
-    console.log("synchronous code")
-    throw new Error("some error occured")
     res.json({message: "API running..."})
 })
 
-app.get('/a', (req,res,next) => {
-    setTimeout(() => {
-        try {
-            aconsole.log("asynchronouse code");
-        } catch (er) {
-            next(er);
-        }
-    },1000)
-    // next(new Error("some error occured"));
-})
+// mongodb connection
+const connectDB = require("./config/db")
+connectDB();
 
 app.use('/api', apiRoutes)
 
