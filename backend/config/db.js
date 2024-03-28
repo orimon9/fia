442ed03235eff6bc1471651ff1df7200ef453bcd
or-imon <orimon9@gmail.com>
2024-03-28 03:00:53 +0200

    update server setup ,product model, products controller,config, env,mongosse,nodemon package install

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
-const mongoose =require("mongoose");
+const mongoose = require("mongoose");
+
 
 const connectDB = async() => {
+
 
     try{
         await mongoose.connect(process.env.MONGO_URI,{
