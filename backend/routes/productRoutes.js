7b12ef23fff97fa0973e42ac3b0ed01882fee4f4
or-imon <orimon9@gmail.com>
2024-04-30 23:30:30 +0300

    update productController, productRoutes and pagination

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,19 @@
 const express = require('express')
-
 const router = express.Router()
+const {getProducts, getProductById, getBestsellers, adminGetProducts, adminDeleteProduct, adminCreateProduct, adminUpdateProduct} = require("../controllers/productController")
 
-const getProducts = require('../controllers/productController')
+router.get("/category/:categoryName/search/:searchQuery", getProducts)
+router.get("/category/:categoryName", getProducts)
+router.get("/search/:searchQuery", getProducts)
+router.get("/", getProducts)
+router.get("/bestsellers", getBestsellers)
+router.get("/get-one/:id", getProductById)
 
-router.get("/",getProducts)
+// admin routes:
+router.get("/admin", adminGetProducts)
+router.delete("/admin/:id", adminDeleteProduct)
+router.put("/admin/:id", adminUpdateProduct)
+router.post("/admin", adminCreateProduct)
 
 module.exports = router
+/
